b3b6bfd30137f07a7c7bca7cb7d1d522ac3636fb
Luca Cantù <cantuluca92@hotmail.com>
2023-04-04 16:20:57 +0200

    finished chart

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,11 +1,32 @@
-import React from "react";
-import './NewExpense.css'
+import React, {useState} from "react";
+import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
-const NewExpense = () =>{
-    return  <div className="new-expense">
-                <ExpenseForm></ExpenseForm>
-            </div>
+const NewExpense = (props) => {
+    const [isEditing, setIsEditing] = useState(false)
+
+  const SaveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+    setIsEditing(false)
+  };
+
+  const startEditingHandler =() =>{
+    setIsEditing(true)
+  }
+
+  const stopEditingHandler= ()=>{
+    setIsEditing(false)
+  }
+  return (
+    <div className="new-expense">
+      {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+      {isEditing && <ExpenseForm onSaveExpenseData={SaveExpenseDataHandler} onCancel={stopEditingHandler} />}
+    </div>
+  );
 };
 
-export default NewExpense
+export default NewExpense;
